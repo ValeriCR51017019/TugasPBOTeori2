86a10b1c26d31c37278d547bd5674870c817822e
ValeriCR51017019 <48231242+ValeriCR51017019@users.noreply.github.com>
2019-10-02 21:09:06 +0800

    Add files via upload

---
+++ Nilaimahasiswa.java
@@ -0,0 +1,64 @@
+/**
+ * @(#)Nilaimahasiswa.java
+ *
+ *
+ * @author 
+ * @version 1.00 2019/10/2
+ */
+
+
+import java.util.Scanner;
+public class Nilaimahasiswa
+{
+	public static void main(String[] args)
+	{
+		Scanner in= new Scanner(System.in);
+		char nilaihuruf;
+		String nama;
+		double nilaitugas1,nilaitugas2,nilaitugas3,nilaimid,nilaifinal;
+		double nilairata2tugas;
+		double nilaiakhir;
+		
+		System.out.print("Nama         : ");
+		nama=in.nextLine();
+		System.out.print("nilai tugas 1: ");
+		nilaitugas1=in.nextInt();
+		System.out.print("nilai tugas 2: ");
+		nilaitugas2=in.nextInt();
+		System.out.print("nilai tugas 3: ");
+		nilaitugas3=in.nextInt();
+		System.out.print("nilai mid    : ");
+		nilaimid=in.nextInt();
+		System.out.print("nilai final  : ");
+		nilaifinal=in.nextInt();
+		System.out.println();
+		
+		nilairata2tugas=(nilaitugas1+nilaitugas2+nilaitugas3)/3;
+		nilaiakhir=(nilairata2tugas*0.3)+(nilaimid*0.3)+(nilaifinal*0.4);
+		
+			if(nilaiakhir>=80)
+		{
+			nilaihuruf='A';
+		}
+			else if(nilaiakhir>=60 && nilaiakhir<=79.9)
+		{
+			nilaihuruf='B';
+		}
+			else if(nilaiakhir>=50 && nilaiakhir<=59.9)
+		{
+			nilaihuruf='C';	
+		}
+			else if(nilaiakhir>=40 && nilaiakhir<=49.9)
+		{
+			nilaihuruf='D';
+		}
+			else
+		{
+			nilaihuruf='E' ;
+		}
+		
+		System.out.println("nama       : "+nama);
+		System.out.println("nilai akhir: "+nilaiakhir);
+		System.out.println("nilai huruf: "+nilaihuruf);		
+	}
+}
